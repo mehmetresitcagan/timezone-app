ad8d10387bb521d3cf1431e9d3b6ccbff2d99eaa
mehmetresitcagan <mresitcagan@gmail.com>
2024-02-22 21:48:23 +0300

    finished

--- js/main.js
+++ js/main.js
@@ -1,5 +1,25 @@
-var now = dayjs()
+function displayDate() {
+    const formattedDate = dayjs().format('dddd, D MMMM, YYYY');
+
+    var dateElement = document.getElementById('date');
+
+    dateElement.innerHTML = formattedDate;
+}
 
 
-console.log(now);
+function displayTime() {
+    // setInterval kullanarak her saniyede bir zamanı güncelle
+    setInterval(() => {
 
+        // Saat, dakika ve saniye bilgilerini al
+        const currentTime = dayjs().format('HH:mm:ss');
+
+        let timeElement = document.getElementById('time');
+
+        timeElement.innerHTML = currentTime;
+    }, 1000); // Her 1000 milisaniyede bir (1 saniye) güncelle
+}
+
+
+displayTime();
+displayDate();
